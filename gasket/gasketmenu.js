362f7eb63f7ee8be6b8f66c4520f981444343ec5
Max <jmhuddle4@gmail.com>
2015-07-22 14:09:39 -0400

    Create gasketmenu.js

---
+++ gasket/gasketmenu.js
@@ -0,0 +1,179 @@
+var gl;
+var points = [];
+var canvas;
+var NumPoints = 5000;
+var NumTimesToSubdivide = 5;
+
+window.onload = func;
+function func(){
+   if(document.getElementById("options").value == "Random"){  
+       window.document.title = "Random Gasket";
+       Random();
+   }
+   else if(document.getElementById("options").value == "Recursive"){
+       window.document.title =  "Recursive Gasket";
+       Recursive();
+   }
+    else if (document.getElementById("options").value == "Exit"){
+        
+        window.close();//ends program
+    }//end if else
+        
+}//end func
+
+//gasket1
+function Random() {
+    canvas = document.getElementById("gl-canvas");
+
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) { alert("WebGL isn't available"); }
+
+    //
+    //  Initialize our data for the Sierpinski Gasket
+    //
+
+    // First, initialize the corners of our gasket with three points.
+
+    var vertices = [
+        vec2(-1, -1),
+        vec2(0, 1),
+        vec2(1, -1)
+    ];
+
+    // Specify a starting point p for our iterations
+    // p must lie inside any set of three vertices
+
+    var u = add(vertices[0], vertices[1]);
+    var v = add(vertices[0], vertices[2]);
+    var p = scale(0.25, add(u, v));
+
+    // And, add our initial point into our array of points
+
+    points = [p];
+
+    // Compute new points
+    // Each new point is located midway between
+    // last point and a randomly chosen vertex
+
+    for (var i = 0; points.length < NumPoints; ++i) {
+        var j = Math.floor(Math.random() * 3);
+        p = add(points[i], vertices[j]);
+        p = scale(0.5, p);
+        points.push(p);
+    }
+
+    //
+    //  Configure WebGL
+    //
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
+    //  Load shaders and initialize attribute buffers
+
+    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    // Load the data into the GPU
+
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+
+    // Associate out shader variables with our data buffer
+
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    renderRand();
+};
+
+
+function renderRand() {
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.POINTS, 0, points.length);
+}
+
+
+//gasket2
+function Recursive() {
+    points = [];
+    canvas = document.getElementById("gl-canvas");
+
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) { alert("WebGL isn't available"); }
+
+    //
+    //  Initialize our data for the Sierpinski Gasket
+    //
+
+    // First, initialize the corners of our gasket with three points.
+
+    var vertices = [
+        vec2(-1, -1),
+        vec2(0, 1),
+        vec2(1, -1)
+    ];
+
+    divideTriangle(vertices[0], vertices[1], vertices[2],
+                    NumTimesToSubdivide);
+
+    //
+    //  Configure WebGL
+    //
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
+    //  Load shaders and initialize attribute buffers
+
+    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    // Load the data into the GPU
+
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+
+    // Associate out shader variables with our data buffer
+
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    renderRecur();
+};
+
+function triangle(a, b, c) {
+    points.push(a, b, c);
+}
+
+function divideTriangle(a, b, c, count) {
+
+    // check for end of recursion
+
+    if (count === 0) {
+        triangle(a, b, c);
+    }
+    else {
+
+        //bisect the sides
+
+        var ab = mix(a, b, 0.5);
+        var ac = mix(a, c, 0.5);
+        var bc = mix(b, c, 0.5);
+
+        --count;
+
+        // three new triangles
+
+        divideTriangle(a, ab, ac, count);
+        divideTriangle(c, ac, bc, count);
+        divideTriangle(b, bc, ab, count);
+    }
+}
+
+function renderRecur() {
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLES, 0, points.length);
+}
