eae056450a8d3da8df3be4fbc4eb76f45db7b1c1
Max <jmhuddle4@gmail.com>
2016-08-09 21:46:02 -0400

    add function changeImage

--- lexie/javascript/portfolio.js
+++ lexie/javascript/portfolio.js
@@ -1,3 +1,14 @@
-<script>
+ function changeImage() {
 
-</script>
+    if (document.getElementById("imgToChange").src == "https://66.media.tumblr.com/c379d02270d7c0d34a913882d2bc790e/tumblr_obk6y4EVJr1tbed15o2_1280.png") 
+    {
+        document.getElementById("imgToChange").src = "https://66.media.tumblr.com/fbcb7869bdb93a520b5fbfa7a3c0768a/tumblr_obk6y4EVJr1tbed15o1_1280.png";
+        document.body.style.background = '#ddf1f0';
+    }
+    else 
+    {
+        document.getElementById("imgToChange").src = "https://66.media.tumblr.com/c379d02270d7c0d34a913882d2bc790e/tumblr_obk6y4EVJr1tbed15o2_1280.png";
+        document.body.style.background = '#221f5a';
+    }
+
+}
