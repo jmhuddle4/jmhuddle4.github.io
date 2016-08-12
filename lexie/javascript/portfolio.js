a3f7c07341a65bcd7a670368fcdfaca14dc460a8
Max <jmhuddle4@gmail.com>
2016-08-11 21:55:29 -0400

    add footer, add scrolling functionality to anchor tag click

--- lexie/javascript/portfolio.js
+++ lexie/javascript/portfolio.js
@@ -1,4 +1,4 @@
- function changeImage() {
+function changeImage() {
 
     if (document.getElementById("imgToChange").src == "https://66.media.tumblr.com/c379d02270d7c0d34a913882d2bc790e/tumblr_obk6y4EVJr1tbed15o2_1280.png") 
     {
@@ -10,5 +10,29 @@
         document.getElementById("imgToChange").src = "https://66.media.tumblr.com/c379d02270d7c0d34a913882d2bc790e/tumblr_obk6y4EVJr1tbed15o2_1280.png";
         document.body.style.background = '#221f5a';
     }
-
 }
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
