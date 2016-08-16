c020167eb752613a55a8ab2e56f1ca877239e030
Max <jmhuddle4@gmail.com>
2016-08-15 22:22:19 -0400

    change background colors

--- lexie/javascript/portfolio.js
+++ lexie/javascript/portfolio.js
@@ -3,13 +3,13 @@
     if (document.getElementById("imgToChange").src == "https://66.media.tumblr.com/c379d02270d7c0d34a913882d2bc790e/tumblr_obk6y4EVJr1tbed15o2_1280.png") 
     {
         document.getElementById("imgToChange").src = "https://66.media.tumblr.com/fbcb7869bdb93a520b5fbfa7a3c0768a/tumblr_obk6y4EVJr1tbed15o1_1280.png";
-        document.body.style.background = '#ddf1f0';
+        document.body.style.background = '#fbf9f9';
         document.body.style.color = "black";
     }
     else 
     {
         document.getElementById("imgToChange").src = "https://66.media.tumblr.com/c379d02270d7c0d34a913882d2bc790e/tumblr_obk6y4EVJr1tbed15o2_1280.png";
-        document.body.style.background = '#221f5a';
+        document.body.style.background = '#59a063';
         document.body.style.color = "white";
     }
 
