6ef263dbadcaa30633fc81cea051284fe750de3b
Max <jmhuddle4@gmail.com>
2016-09-13 22:56:30 -0400

    make radiotopia part of a gallery

--- lexie/javascript/portfolio.js
+++ lexie/javascript/portfolio.js
@@ -18,6 +18,27 @@
 
 }
 
+var index = 0;
+var images = ["https://67.media.tumblr.com/89324d5e3aad4d926c6c7d3bcc8a77ca/tumblr_obru0k0y6Q1uzg32xo1_540.png",
+               "https://67.media.tumblr.com/729012ba8b9c80f1df88e89676028d6a/tumblr_obru0k0y6Q1uzg32xo2_1280.png",
+               "https://67.media.tumblr.com/4576fffff4d753528cfc098f9a12c613/tumblr_obru0k0y6Q1uzg32xo3_1280.png",
+               "https://67.media.tumblr.com/0144cb6fc17b59fea24b678a94bf9ed5/tumblr_obru0k0y6Q1uzg32xo4_1280.png",
+               "https://65.media.tumblr.com/6fbcc27908731137ba9aef4cd3baf691/tumblr_obru0k0y6Q1uzg32xo5_1280.png"];
+
+function slideImageRight() {
+    if(index != images.length - 1) {
+        document.getElementById("gallery1").src = images[index + 1]
+        index++;
+    }
+}
+
+function slideImageLeft() {
+    if(index != 0) {
+        document.getElementById("gallery1").src = images[index - 1]
+        index--;
+    }
+}
+
  $(document).ready(function(){
       // Add smooth scrolling to all links
       $("a").on('click', function(event) {
