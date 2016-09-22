7d62c8de98cb9c7bb3d38b9d71426a81d34129f2
Max <jmhuddle4@gmail.com>
2016-09-21 23:21:18 -0400

    fix header, change navbar color on click

--- lexie/javascript/portfolio.js
+++ lexie/javascript/portfolio.js
@@ -1,45 +1,5 @@
- // fade in page load
- $(document).ready(function(){$("body").hide().fadeIn(1000);});
-
- function changeImage() {
-    //2nd image
-    if (document.getElementById("imgToChange").src == "https://66.media.tumblr.com/6c57a17efb5ce610e572ccffa20a2116/tumblr_ocaba5cInB1uzg32xo1_540.png")
-    {
-        //1st image
-        document.getElementById("imgToChange").src = "https://67.media.tumblr.com/2100b64831891821cbe543895e736484/tumblr_odvv47EYmj1uzg32xo1_1280.png";
-        document.body.style.background = '#fbf9f9'; //light
-        document.body.style.color = "#928d88"; //text
-    }
-    else 
-    {
-        //2nd image
-        document.getElementById("imgToChange").src = "https://66.media.tumblr.com/6c57a17efb5ce610e572ccffa20a2116/tumblr_ocaba5cInB1uzg32xo1_540.png";
-        document.body.style.background = '#6fc38e'; //dark color
-        document.body.style.color = "white"; //text
-    }
-}
-
-// gallery logic
-var index = 0;
-var images = ["img/portfolio_site_radiotopia_1.png",
-               "img/portfolio_site_radiotopia_2.png",
-               "img/portfolio_site_radiotopia_3.png",
-               "img/portfolio_site_radiotopia_4.png",
-               "img/portfolio_site_radiotopia_5.png"];
-
-function slideImageRight() {
-    if(index != images.length - 1) {
-        document.getElementById("gallery1").src = images[index + 1]
-        index++;
-    }
-}
-
-function slideImageLeft() {
-    if(index != 0) {
-        document.getElementById("gallery1").src = images[index - 1]
-        index--;
-    }
-}
+// fade in page load
+$(document).ready(function(){$("body").hide().fadeIn(1000);});
 
 // Add smooth scrolling to all links
 $(document).ready(function(){
@@ -82,3 +42,23 @@ $(document).ready(function(){
       }
     });
 });
+
+function changeImage() {
+    //2nd image
+    if (document.getElementById("imgToChange").src == "https://66.media.tumblr.com/6c57a17efb5ce610e572ccffa20a2116/tumblr_ocaba5cInB1uzg32xo1_540.png")
+    {
+        //1st image
+        document.getElementById("imgToChange").src = "https://67.media.tumblr.com/2100b64831891821cbe543895e736484/tumblr_odvv47EYmj1uzg32xo1_1280.png";
+        document.body.style.background = '#fbf9f9'; //light
+        document.body.style.color = "#928d88"; //gray text
+        document.getElementById("navbar").style.background = "#928d88";
+    }
+    else
+    {
+        //2nd image
+        document.getElementById("imgToChange").src = "https://66.media.tumblr.com/6c57a17efb5ce610e572ccffa20a2116/tumblr_ocaba5cInB1uzg32xo1_540.png";
+        document.body.style.background = '#6fc38e'; //dark color
+        document.body.style.color = "white"; //text
+        document.getElementById("navbar").style.background = "white";
+    }
+}
